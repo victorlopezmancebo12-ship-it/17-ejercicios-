8ff402c15ce404bf48d5edd376b276e2e13e8039
victorlopezmancebo12-ship-it <victorlopezmancebo12@gmail.com>
2025-10-01 13:03:54 +0000

    libro

---
+++ ejercicio 17/libro.py
@@ -0,0 +1,20 @@
+from datetime import date, timedelta
+
+class Libro:
+    def __init__(self, id_libro, titulo, autor, tematica):
+        self.id_libro = id_libro
+        self.titulo = titulo
+        self.autor = autor
+        self.tematica = tematica
+        self.ejemplares = 1  # cantidad de ejemplares disponibles
+
+
+
+
+
+
+
+
+
+
+
