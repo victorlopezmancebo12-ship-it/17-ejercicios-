df6c1a16dbf26367860ad48a39d7dff029920438
victorlopezmancebo12-ship-it <victorlopezmancebo12@gmail.com>
2025-10-01 13:04:00 +0000

    persona

---
+++ ejercicio 17/persona.py
@@ -0,0 +1,5 @@
+class Persona:
+    def __init__(self, identificacion, nombre, direccion):
+        self.identificacion = identificacion
+        self.nombre = nombre
+        self.direccion = direccion
