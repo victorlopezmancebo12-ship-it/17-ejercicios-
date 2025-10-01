89d9406e3be1f8fd60f7a1c8397d7de28138026f
victorlopezmancebo12-ship-it <victorlopezmancebo12@gmail.com>
2025-10-01 12:40:05 +0000

    obra de arte

---
+++ ejercicio 3/obra arte.py
@@ -0,0 +1,17 @@
+
+
+class ObraDeArte:
+    def __init__(self, titulo, autor, fecha, tecnica, subtecnica, soporte, descripcion, estado_conservacion, original=None):
+        self.titulo = titulo
+        self.autor = autor
+        self.fecha = fecha
+        self.tecnica = tecnica
+        self.subtecnica = subtecnica
+        self.soporte = soporte
+        self.descripcion = descripcion
+        self.estado_conservacion = estado_conservacion
+        self.original = original  # Referencia a la obra original (si es réplica)
+
+
+
+
