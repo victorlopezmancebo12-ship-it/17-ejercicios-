e00ccad4840d8bae6c30fd8bd6db93930ae47571
victorlopezmancebo12-ship-it <victorlopezmancebo12@gmail.com>
2025-10-01 12:41:22 +0000

    participante

---
+++ ejercicio 7/participante.py
@@ -0,0 +1,18 @@
+
+
+class Participante:
+    """
+    Representa a una persona involucrada en el proyecto.
+    Atributos:
+        nombre (str): Nombre del participante.
+        rol (str): Rol dentro del proyecto (ej. Desarrollador, Analista).
+        experiencia (int): Años de experiencia profesional.
+    """
+    def __init__(self, nombre, rol, experiencia):
+        self.nombre = nombre
+        self.rol = rol
+        self.experiencia = experiencia
+
+
+
+
