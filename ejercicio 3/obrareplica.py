e9b38738632a724fb8dd48093a3cdb50eee0d00d
victorlopezmancebo12-ship-it <victorlopezmancebo12@gmail.com>
2025-10-01 12:40:18 +0000

    replica

---
+++ ejercicio 3/obrareplica.py
@@ -0,0 +1,15 @@
+gioconda_replicada = ObraDeArte(
+    titulo="La Gioconda (Réplica del Prado)",
+    autor="Anónimo (alumno de la escuela de Leonardo)",
+    fecha="1503-1516",
+    tecnica="óleo",
+    subtecnica="pincelada simple",
+    soporte="madera de nogal",
+    descripcion=(
+        "Réplica más antigua conocida, procedente de las Colecciones Reales, "
+        "expuesta en el Museo del Prado. Realizada por un discípulo de Leonardo "
+        "al mismo tiempo que la original. Mejor estado de conservación que la original."
+    ),
+    estado_conservacion="muy bueno",
+    original=gioconda_original
+)
