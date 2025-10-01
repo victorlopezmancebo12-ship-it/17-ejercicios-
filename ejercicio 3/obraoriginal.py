e829c9c53ddf1236790658ec6bbcb2ea4ff801aa
victorlopezmancebo12-ship-it <victorlopezmancebo12@gmail.com>
2025-10-01 12:40:13 +0000

    original

---
+++ ejercicio 3/obraoriginal.py
@@ -0,0 +1,10 @@
+gioconda_original = ObraDeArte(
+    titulo="La Gioconda",
+    autor="Leonardo Da Vinci",
+    fecha="1503-1516",
+    tecnica="óleo",
+    subtecnica="sfumato",
+    soporte="madera de álamo",
+    descripcion="Obra maestra expuesta en el Museo Louvre de París.",
+    estado_conservacion="regular"
+)
